d93503ac1de205dd041548258610a6fd6267a1f2
vamsi906 <124115031+vamsi906@users.noreply.github.com>
2023-02-03 22:56:41 -0800

    Create v20.c

---
+++ v20.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+using namespace std;
+void countCurrency(int amount)
+{
+int notes[9] = { 2000, 500, 200, 100,50, 20, 10, 5, 1 };
+int noteCounter[9] = { 0 };
+for (int i = 0; i < 9; i++) {
+if (amount >= notes[i]) {
+noteCounter[i] = amount / notes[i];
+amount = amount % notes[i];
+}
+}
+count << "Currency Count ->" << endl;
+for (int i = 0; i < 9; i++) {
+if (noteCounter[i] != 0) {
+cout << notes[i] << " : "
+<< noteCounter[i] << endl;
+cout<<notes[i]+notes[i+1];
+}
+}
+}
+int main()
+{
+int amount = 868;
+countCurrency(amount);
+return 0;
+}
